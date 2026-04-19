3e99592f1cad33dfc22f7dd5cfe9f6d41c11ce3b
piasmajumdar <piasmajumdar@gmail.com>
2026-04-19 18:23:03 +0530

    Add react-tabs for tabbed navigation in Books component

--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -1,14 +1,27 @@
 import React, { useContext } from 'react';
 import { BookContext } from '../../context/BookContext';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Books = () => {
-    const {storedBooks, wishList} = useContext(BookContext);
-    console.log(storedBooks,wishList, "Book Context")
+    const { storedBooks, wishList } = useContext(BookContext);
+    console.log(storedBooks, wishList, "Book Context")
     return (
-        <div className='w-11/12 mx-auto'>
-            read list: {storedBooks.length} <br />
-            wish list: {wishList.length}
-            
+        <div className='w-11/12 mx-auto my-3'>
+            <Tabs>
+                <TabList>
+                    <Tab>Read List</Tab>
+                    <Tab>Wish List</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>read list: {storedBooks.length} </h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>wish list: {wishList.length}</h2>
+                </TabPanel>
+            </Tabs>
+
         </div>
     );
 };
